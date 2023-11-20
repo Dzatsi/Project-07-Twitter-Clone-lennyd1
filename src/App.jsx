b7b672c749223ff3bd9c9fcaf9201f8ed1dc4cd4
Dzatsi <135968595+Dzatsi@users.noreply.github.com>
2023-11-20 10:05:27 +0000

    second variable for tchek project

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Layout from "./components/layout.jsx";
 import('./style/reset.css');
 import('./style/App.css');
 const falseVariable = 'Leteta';
+const secondVariable = 'lenny'
 export default function App() {
   return (
     <Layout>
