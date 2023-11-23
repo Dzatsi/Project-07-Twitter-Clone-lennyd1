3bfeb42d9e8e3c7fefd40ac115fb15227d3e66c1
lenny dzatsi <lennydzatsi8@gmail.com>
2023-11-23 15:24:32 +0100

    add somes functions for the clone the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Home from "./pages/home.jsx";
 import Layout from "./components/layout.jsx";
+import Header from "./components/Header.jsx";
 
 import('./style/reset.css');
 import('./style/App.css');
-const falseVariable = 'Leteta';
-const secondVariable = 'lenny'
+
 export default function App() {
   return (
     <Layout>
-      <Home />
+      <Header />
+      {/* <Home /> */}
     </Layout>
   );
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+export default function Header () {
+    return (
+        <div className="header">
+            <div className="page-title">
+             <h1>Home</h1>
+            </div>
+            <div className="top-tweets">
+                <img src="../images/shining.png" alt="logo for popular tweet" />
+            </div>
+        </div>        
+    )
+}
