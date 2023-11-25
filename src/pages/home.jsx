08103546fcb238dbb014aa43d89765ed84802869
lenny dzatsi <lennydzatsi8@gmail.com>
2023-11-25 09:06:30 +0100

    finish all main components

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,6 +5,7 @@ import Tweets from '../components/tweets';
 import feuille from "../images/feuille.png"
 import profil from "../images/profil.png"
 import image from "../images/image.svg"
+import twitter from "../images/twitter.svg"
 
 function Home() {
   return (
@@ -21,6 +22,21 @@ function Home() {
         <p>
           Gardening boomed during the pandemic. Six Black writers share how has heped them re-establish, and reimagine, a connection to 
           cultivation and the land
+        </p>
+      </Tweets>
+      <Tweets title ="Twitter" detail ="@Twitter . Oct 29" profil={twitter} details="6.8K" id="36.6K" content="367.1K" >
+        <p>
+          BIG NEWS lol jk still Twitter 
+        </p>
+      </Tweets>
+      <Tweets title ="Twitter" detail ="@Twitter . Oct 4" profil={twitter} details="118.7K" id="785.4K" content="3.3M" >
+        <p>
+          Hello literally every one 
+        </p>
+      </Tweets>
+      <Tweets title ="Twitter" detail ="@Twitter . Oct 4" profil={twitter} details="98K" id="74.6K" content="1.1M" >
+        <p>
+          Hello literally every one 
         </p>
       </Tweets>
     </main>
